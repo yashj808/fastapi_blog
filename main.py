84f72f01d8e28aff68630058c2f12720bd066c2f
Yash Jogdand <yashjogdandyj@gmail.com>
2026-01-26 15:33:06 +0530

    Config-StarletteHandler

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status, HTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
@@ -30,7 +33,46 @@ posts: list[dict] = [
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts":posts})
 
+@app.get("/posts/{post_id}", include_in_schema=False)
+def post_page(request:Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post["title"][:50]
+            return templates.TemplateResponse(request, "post.html", {"post":post, "title":title})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post not found")
+
 @app.get("/api/posts/")
 def get_posts():
     return posts
 
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post not found")
+
+## StarletteHTTPException Handler
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
+    message = (
+        exception.detail
+        if exception.detail
+        else "An error occurred. Please check your request and try again."
+    )
+
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exception.status_code,
+            content={"detail": message},
+        )
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": exception.status_code,
+            "title": exception.status_code,
+            "message": message,
+        },
+        status_code=exception.status_code,
+    )
