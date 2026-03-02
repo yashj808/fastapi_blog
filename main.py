801fcc90ef8b7afe63a4b517e2a0c1b4acf9c203
Yash Jogdand <yashjogdandyj@gmail.com>
2026-03-02 21:40:43 +0530

    DeleteUser-Feature

--- main.py
+++ main.py
@@ -211,6 +211,18 @@ def update_user_full(user_id: int,
                     db.refresh(user)
                     return user
 
+@app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+         raise HTTPException(
+              status_code=status.HTTP_404_NOT_FOUND,
+              detail="User not found",
+         )
+    db.delete(user)
+    db.commit()
+
 
 @app.get("/api/posts", response_model=list[PostResponse])
 def get_posts(db: Annotated[Session, Depends(get_db)]):
